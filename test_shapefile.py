afb599023d9cdb7bcf604a6e8e989d2b7d52548a
skeptycal <skeptycal@gmail.com>
2019-04-18 21:22:47 -0500

    shapefile test script

--- test_shapefile.py
+++ test_shapefile.py
@@ -13,39 +13,60 @@ else:
 def init_shapefile(filename: str = 'shapefile.shp') -> sh.Reader:
     """ read_shapefile - open and read a shapefile
 
-    Keyword Arguments:
-        filename {str} -- file name (default: {'shapefile.shp'})
+        Keyword Arguments:
+            filename {str} -- file name (default: {'shapefile.shp'})
 
-    Returns:
-        {shapefile} -- returns the reader object for the
-                       shapefile that was initialized
-    """
-    e:BaseException.args = []
-    shape_test: sh.Reader = Null
+        Returns:
+            {shapefile} -- returns the reader object for the
+                           shapefile that was initialized
+        """
+    init_shape: sh.Reader = None
     try:
-        shape_test = sh.Reader(filename)
-    except FileNotFoundError:
-        print("The shapefile (", filename, "was not found.")
-    except TypeError:
-        print("The shapefile (", filename, "is the wrong type (", type(filename), ").")
-    except ValueError:
-        print("The shapefile (", filename, "has a badly formatted filename.")
+        init_shape = sh.Reader(filename)
+    except FileNotFoundError as e:
+        print("The shapefile (", filename, ") was not found.")
+        print(e.args)
+    except TypeError as e:
+        print("The shapefile (", filename, ") is the wrong type (", type(filename), ").")
+        print(e.args)
+    except ValueError as e:
+        print("The shapefile (", filename, ") has a badly formatted filename.")
+        print(e.args)
     except OSError as e:
-        print("The shapefile (", filename, "caused an OS error.")
-    except Exception as e:
-        print("The shapefile (", filename, "caused an unknown error.")
-    except BaseException as e:
-        print("The shapefile (", filename, "caused an unknown system error.")
+        print("The shapefile (", filename, ") caused an OS error.")
+        print(e.args)
+    except Exception as e:      # pylint: disable=broad-except
+        print("The shapefile (", filename, ") caused an unknown error.")
+        print(e.args)
     else:
-        print("Reading shapefile (", filename, ") of type (", type(filename), ".")
-    finally:
-        return shape_test
+        print("Reading shapefile (", filename, ") of type (", type(filename), ").")
+    return init_shape
+
+def load_shapes(load_shape):
+    """ read_shapefile - open and read a shapefile
+
+        Keyword Arguments:
+            filename {str} -- file name (default: {'shapefile.shp'})
+
+        Returns:
+            {shapefile} -- returns the reader object for the
+                           shapefile that was initialized
+        """
+    load_result = load_shape
+    return load_result
 
 
 if __name__ == "__main__":
     """ Tests for shapefile module (if run from CLI) """
     test_file: str = 'shapefile.shp'
-
-    reader: sh.Reader = init_shapefile(test_file)
-    if reader.
+    test_reader: sh.Reader = init_shapefile(test_file)
+    print("Encoded name of shapefile: ", test_reader.shapeName)
+    print("Number of records: ", test_reader.numRecords)
+    print()
+    print("Record at index[0]: ", test_reader.record(0))
+    print("Shape at index[0]: ", test_reader.sh)
+    test_shapes: sh.Reader.iterShapes = test_reader.iterShapes
+    test_records: sh.Reader.iterRecords = test_reader.iterRecords
+    print(len(test_shapes))
+    # if reader.
     # continue ...
